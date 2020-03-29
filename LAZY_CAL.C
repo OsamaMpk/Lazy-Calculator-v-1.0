a1bbecb1ee371467c1107c4dd28f3a93f7b183b7
Muhammad Osama Awan <57126125+OsamaMpk@users.noreply.github.com>
2020-03-30 02:49:26 +0500

    some new fixes

--- LAZY_CAL.C
+++ LAZY_CAL.C
@@ -10,50 +10,52 @@ char oper,q_char;
 start_1:
   clrscr();
   delay(1000);
-  printf("Welcome to the Lazy Calculator by @osama");
-  delay(500);
+  printf("*----------------------------------------*\n");
+  printf("|Welcome to the Lazy Calculator by @osama|\n");
+  printf("*----------------------------------------*\n");
+  delay(1000);
   printf("\nLet's enter the Value One:\t");
   scanf("%d", &num_1);
-  delay(500);
+  delay(1000);
   printf("value stored!");
-  delay(500);
+  delay(1000);
   printf("\nLet's enter the value Two:\t");
   scanf("%d", &num_2);
-  delay(500);
+  delay(1000);
   printf("value stored!");
-  delay(500);
+  delay(1000);
   printf("\nWait! Wait! Wait! Let's put Operation:\t");
-  oper = getch();
+  oper = getche();
   delay(1000);
   printf("\nLazy Calculator claculating value Wait!");
-  delay(500);
+  delay(1000);
   printf("\nWait! result's are approximately ready.");
-  delay(500);
+  delay(1000);
   switch ( oper ){
    case '+' :
      calc = num_1 + num_2;
-     printf("\nAdding this %d in %d is %f", num_1, num_2, calc);
+     printf("\nAdding this %d in %d is %.0f", num_1, num_2, calc);
      break;
    case '-' :
      if(num_1 <= num_2){
        calc = num_2 - num_1;
-       printf("\nSubtracting %d from %d is %f", num_1, num_2, calc);
+       printf("\nSubtracting %d from %d is %.2f", num_1, num_2, calc);
      }else{
        calc = num_1 - num_2;
-       printf("\nSubtracting %d from %d is %f", num_2, num_1, calc);
+       printf("\nSubtracting %d from %d is %.2f", num_2, num_1, calc);
      }
      break;
    case '*' :
      calc = num_2 * num_2;
-     printf("\nMultpling this %d with %d is %f", num_1, num_2, calc);
+     printf("\nMultpling this %d with %d is %.0f", num_1, num_2, calc);
      break;
    case '/' :
      if(num_1 <= num_2){
        calc = num_2 / num_1;
-       printf("\nDividing %d on %d is %f", num_2, num_1, calc);
+       printf("\nDividing %d on %d is %.2f", num_2, num_1, calc);
      }else{
        calc = num_1 / num_2;
-       printf("\nDividing %d on %d is %f", num_1, num_2, calc);
+       printf("\nDividing %d on %d is %.2f", num_1, num_2, calc);
      }
      break;
    default :
@@ -64,12 +66,13 @@ start_1:
   start_2:
     delay(1000);
     printf("\nThanks for Using Lazy calculator.");
-    delay(300);
-    printf("\nDid you want to re-try press yes (y) otherwise no (n)");
-    q_char = getch();
+    delay(1000);
+    printf("\nDid you want to re-try press yes (y) otherwise no (n)\t");
+    q_char = getche();
+    delay(1000);
     if(q_char == 'y'){
       printf("\nThanks for giving us Second try.");
-      delay(500);
+      delay(1000);
       printf("\nWe are Setting things for you.");
       delay(1000);
       goto start_1;
@@ -78,8 +81,8 @@ start_1:
     }
 end_1:
   delay(1000);
-  printf("\nWe are sorry you won't happy with our program we try best to improve this claculator.");
-  delay(400);
-  printf("\nBye, bye.");
+  printf("\nWe are sorry you won't happy with our program we try best to improve this calculator.");
+  delay(1000);
+  printf("\nBye, bye. ");
   getch();
 }
